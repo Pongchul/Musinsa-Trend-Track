c62083f2e0f0e041cce08253dd557f5def4af90e
inchul7511@gmail.com <inchul7511@gmail.com>
2026-05-07 14:33:38 +0900

    Refactoring : TopBar에서 링크 클릭시 해당 페이지로 이동할 수 있도록 라우터 축가

--- frontend/src/components/TopBar/TopBar.tsx
+++ frontend/src/components/TopBar/TopBar.tsx
@@ -1,11 +1,14 @@
 import {Avatar, IconBtn, Logo, LogoIcon, Nav, NavLink, NavRight, SearchBar} from "./TopBar.styled.ts";
 import {FaArrowTrendUp, FaMagnifyingGlass} from "react-icons/fa6";
 import {FaBell} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const TopBar = () => {
+    const navigate = useNavigate();
+
     return (
         <Nav>
-            <Logo>
+            <Logo onClick={() => navigate("/")}>
                 <LogoIcon>
                     <FaArrowTrendUp size={20} />
                 </LogoIcon>
@@ -13,22 +16,22 @@ const TopBar = () => {
             </Logo>
 
             <SearchBar>
-               <FaMagnifyingGlass /> Search brands, items...
+                <FaMagnifyingGlass /> Search brands, items...
             </SearchBar>
 
             <NavRight>
-                <NavLink>Dashboard</NavLink>
-                <NavLink>Analytics</NavLink>
+                <NavLink onClick={() => navigate("/dashboard")}>Dashboard</NavLink>
+                <NavLink onClick={() => navigate("/analytics")}>Analytics</NavLink>
                 <IconBtn>
                     <FaBell size={20} />
                 </IconBtn>
                 <Avatar>
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/4751/4751706.png"
-                    alt="profile"
-                    width={28}
-                    height={28}
-                  />
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/4751/4751706.png"
+                        alt="profile"
+                        width={28}
+                        height={28}
+                    />
                 </Avatar>
             </NavRight>
         </Nav>

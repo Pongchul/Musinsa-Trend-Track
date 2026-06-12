dd48ff638ae73fd7590109bdf3b495e5648f167d
inchul7511@gmail.com <inchul7511@gmail.com>
2026-06-12 15:31:04 +0900

    Refactoring : tanstack react query 사용을 위한 QueryClientProvider 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from "react-router-dom";
 import App from './App.tsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5,  // 5분 캐시
+            retry: 1,                   // 실패 시 1번만 재시도
+        },
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <HashRouter basename="/">
-            <App />
-        </HashRouter>
-    </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+        <StrictMode>
+            <HashRouter basename="/">
+                <App />
+            </HashRouter>
+        </StrictMode>
+    </QueryClientProvider>
 )
